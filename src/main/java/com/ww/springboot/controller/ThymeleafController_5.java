b3d2968019f50ab572cb9681c719b7c656c5e7ee
lww <1013232063@qq.com>
2020-09-23 15:08:58 +0800

    2020年9月23日15:08:49

---
+++ src/main/java/com/ww/springboot/controller/ThymeleafController_5.java
@@ -0,0 +1,25 @@
+package com.ww.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author linweiwei
+ * @version 1.0
+ * @date 2020-09-21 15:32
+ * @describe:   模板引擎
+ */
+
+@Controller
+@RequestMapping("/thymeleaf")
+public class ThymeleafController_5 {
+
+    @RequestMapping("test1")
+    public String test1(Model model) {
+        model.addAttribute("msg", "Hello,Thymeleaf");
+        //返回到resources/templates/index.html
+        return "index";
+    }
+
+}
